6c38fa989176efb787e2fc71307b35df1b4202c8
sh0w0911 <55259691+sh0w0911@users.noreply.github.com>
2021-04-18 20:00:22 -0700

    Update send_test_email.py

--- send_test_email.py
+++ send_test_email.py
@@ -4,8 +4,8 @@ def send_simple_message():
 		auth=("api", "82c4b9fd1cbae184e3defe7c686a5cbd-a09d6718-76d4880f"),
 		data={"from": 'hello@example.com',
 			"to": ["suyeob.kim@sjsu.edu"],
-			"subject": "Hello",
-			"text": "Testing some Mailgun awesomness!"})
+			"subject": "Set up email notification",
+			"text": "Your Rasberry Pi set up successfully!"})
 
 request = send_simple_message()
 print ('status: ' + format(request.status_code))
