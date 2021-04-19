c4562ebcae1225a1b086faef5cef2245c4a34b5d
sh0w0911 <55259691+sh0w0911@users.noreply.github.com>
2021-04-18 19:54:57 -0700

    set up email notifications

---
+++ send_test_email.py
@@ -0,0 +1,12 @@
+def send_simple_message():
+	return requests.post(
+		"https://https://api.mailgun.net/v3/sandbox7fb0153697a44970b7a430530ca48afc.mailgun.org/messages",
+		auth=("api", "82c4b9fd1cbae184e3defe7c686a5cbd-a09d6718-76d4880f"),
+		data={"from": ¡®hello@example.com¡¯,
+			"to": ["suyeob.kim@sjsu.edu"],
+			"subject": "Hello",
+			"text": "Testing some Mailgun awesomness!"})
+
+request = send_simple_message()
+print ('status: ' + format(request.status_code))
+print ('Body: ' + format(request.text))
